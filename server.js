90f418925260512c1d96fc14707077cd360f861c
Ifeanyi <chimaifeanyi29@gmail.com>
2021-12-18 15:08:00 +0100

    deploying in 5, 4, 3, 2, 1

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.get("/", (req, res) => {
 
 
 
-// const MONGO_URI = "mongodb+srv://mongotut:testing123@cluster0.kogqa.mongodb.net/react-login-DB?retryWrites=true&w=majority"
-const MONGO_URI = "mongodb://localhost:27017/react-login-DB"
+const MONGO_URI = "mongodb+srv://mongotut:testing123@cluster0.kogqa.mongodb.net/react-login-DB?retryWrites=true&w=majority"
+// const MONGO_URI = "mongodb://localhost:27017/react-login-DB"
 
 
 //connect to mongodb
